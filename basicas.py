f6f09de1691950e5430d3ef51958efd9213bb640
juan_venegas <juan.venegas.f@mail.pucv.cl>
2023-11-04 13:00:30 -0300

    Creación de función multiplicación con docstring

---
+++ basicas.py
@@ -0,0 +1,16 @@
+# Multiplicación
+def multiplicacion(a, b):
+  """
+  Esta función devuelve el producto entre a y b.
+
+  Args:
+  input_string (str): El string en el que se contarán los caracteres.
+
+  Returns:
+  int: El número de caracteres en el string de entrada.
+
+  Ejemplo:
+  >>> count_characters("Hola, mundo!")
+  12
+  """
+  return a * b
