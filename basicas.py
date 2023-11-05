4aa5818ca25bf74582ad1467ebfc2c482e383f54
juan_venegas <juan.venegas.f@mail.pucv.cl>
2023-11-05 20:19:11 -0300

    Creación de función potencia con docstring

---
+++ basicas.py
@@ -0,0 +1,16 @@
+# Potencia
+def potencia(base, exponente):
+  """
+  Esta función calcula el valor de una potencia.
+
+  Args:
+  base, exponente (num): base es elevado a exponente.
+
+  Returns:
+  (num): base multiplicado por sí mismo tantas veces como indique exponente.
+
+  Ejemplo:
+  >>> potencia(2,3)
+  8
+  """
+  return base ** exponente
