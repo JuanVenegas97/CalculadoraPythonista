5fc4039962a01af3fcc7a75e3a2c744ed81ced49
juan_venegas <juan.venegas.f@mail.pucv.cl>
2023-11-05 17:27:45 -0300

    Creación de función suma con docstring

---
+++ basicas.py
@@ -0,0 +1,16 @@
+# Suma
+def suma(a, b):
+  """
+  Esta función devuelve la suma de los valores a y b.
+
+  Args:
+  a, b (num): Valores a ser sumados.
+
+  Return:
+  (num): Resultado de la suma de a y b.
+
+  Ejemplo:
+  >>> suma(3,2)
+  5
+  """
+  return a + b
