15e36f5275aa5cb6b784b2cdf059243ccc72da57
juan_venegas <juan.venegas.f@mail.pucv.cl>
2023-11-05 18:37:38 -0300

    Creación de función resta con docstring

---
+++ basicas.py
@@ -0,0 +1,16 @@
+# Resta
+def resta(a, b):
+  """
+  Esta función devuelve la resta entre los valores a y b.
+
+  Args:
+  a, b (num): Valores a ser restados.
+
+  Return:
+  (num): Resultado de la resta de a y b.
+
+  Ejemplo:
+  >>> resta(9, 3)
+  6
+  """
+  return a - b
