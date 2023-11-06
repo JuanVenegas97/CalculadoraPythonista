ff091769874766df0cc50d2ce3b8dad552fb6de8
Claudio Guzmán <claudio.guzman.s01@mail.pucv.cl>
2023-11-06 18:48:07 -0300

    Agregamos la función Arcotangente

--- avanzadas.py
+++ avanzadas.py
@@ -1,26 +1,22 @@
-import math
-# Seno 
-def seno(a,t):
+# Arcotangente 
+def arcotangente(a,t):
   """
-  Esta función calcula la tangente de un ángulo a, cuyo formato de entrada puede ser:
+  Esta función calcula el arcotangente de un número a, cuyo formato de salida puede ser:
   en Radianes (R) o en Grados (D) 
 
   Args:
   a (num), t (char)
 
   Return: 
-  Tangente del ángulo (num)
+  Arcoseno del ángulo (num)
 
   Ejemplo:
-  >>>seno(30,D)
-  0.5
+  >>>arcotangente(1,D)
+  45
   """
   # Condicion que establece el formato de entrada del ángulo
 
   if t == 'R':
-    a = float(a)
+    return atan(float(a))
   else:
-    a = math.radians(float(a))
-	
-  return math.sin(a)
-
+    return atan(float(a))*180/math.pi
