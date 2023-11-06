29901ec7ee97cc5719341de8ed9a310b30e0006a
Claudio Guzmán <claudio.guzman.s01@mail.pucv.cl>
2023-11-06 16:21:49 -0300

    Agregamos la función tangente

---
+++ avanzadas.py
@@ -0,0 +1,26 @@
+import math
+# Tangente 
+def tangente(a,t):
+
+  """
+  Esta función calcula la tangente de un ángulo a, cuyo formato de entrada puede ser:
+  en Radianes (R) o en Grados (D) 
+
+  Args:
+  a (num), t (char)
+
+  Return: 
+  Tangente del ángulo (num)
+
+  Ejemplo:
+  >>>tangente(45,D)
+  1
+  """
+  # Condicion que establece el formato de entrada del ángulo
+
+  if t == 'R':
+    a = float(a)
+  else:
+    a = math.radians(float(a))
+	
+  return math.sin(a)
