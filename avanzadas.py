ebcfe530ab3307d84ec4f37ffeea428fc787db2a
juan_venegas <juan.venegas.f@mail.pucv.cl>
2023-11-06 17:45:52 -0300

    Tercera creación coseno

--- avanzadas.py
+++ avanzadas.py
@@ -1,24 +1,26 @@
-# Seno 
-def seno(a,t):
+import math
+# Coseno 
+def coseno(a,t):
+
   """
-  Esta función calcula el seno de un ángulo a, cuyo formato de entrada puede ser:
+  Esta función calcula el coseno de un ángulo a, cuyo formato de entrada puede ser:
   en Radianes (R) o en Grados (D) 
 
   Args:
   a (num), t (char)
 
   Return: 
-  Seno del ángulo (num)
+  Coseno del ángulo (num)
 
   Ejemplo:
-  >>> seno(30,D)
+  >>>coseno(60,D)
   0.5
   """
   # Condicion que establece el formato de entrada del ángulo
 
-    if t == 'R':
-	a = float(a)
-    else:
-        a = math.radians(float(a))
+  if t == 'R':
+    a = float(a)
+  else:
+    a = math.radians(float(a))
 	
-  return math.sin(a)
+  return math.cos(a)
